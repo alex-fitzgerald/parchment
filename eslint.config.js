85ca7ea26dd8f2f7261373fae25fc47af41aca45
alex-fitzgerald <fitzgerald.s.alexander@gmail.com>
2025-01-03 11:44:50 +1300

    feat: fix lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,32 @@
-import js from "@eslint/js";
-import ts from "typescript-eslint";
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default ts.config(
-  js.configs.recommended,
-  ts.configs.recommendedTypeChecked,
-  ts.configs.stylistic,
-  stylistic.configs["recommended-flat"],
-  stylistic.configs.customize({
-    indent: 4,
-    semi: true
-  }),
-  {
-    plugins: {
-      "@stylistic": stylistic,
+    {
+        ignores: ['dist', 'node_modules', '*.js'],
     },
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    js.configs.recommended,
+    ts.configs.recommendedTypeChecked,
+    ts.configs.stylistic,
+    stylistic.configs['recommended-flat'],
+    stylistic.configs.customize({
+        indent: 4,
+        semi: true,
+    }),
+    {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        languageOptions: {
+            parserOptions: {
+                projectFolderIgnoreList: ['node_modules', 'dist'],
+                projectService: {
+                    defaultProject: 'tsconfig.json',
+                    allowDefaultProject: ['*.js'],
+                },
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-  },
 );
