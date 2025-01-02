c3362fe19e2009df7c1eedd16d60021ce73935ea
alex-fitzgerald <fitzgerald.s.alexander@gmail.com>
2025-01-02 22:28:27 +1300

    feat: add npmrc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default as Parchment } from './components/parchment.tsx';
-export { default as ParchmentLink } from './components/parchment/nav/parchment-nav-button.tsx';
-export { default as ParchmentNav } from './components/parchment/nav/parchment-nav.tsx';
+export { default as ParchmentButton } from './components/parchment-button.tsx';
