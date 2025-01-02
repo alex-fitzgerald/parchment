9e3d8ecd03e4c92321e595de40be05a838245ed9
alex-fitzgerald <fitzgerald.s.alexander@gmail.com>
2025-01-02 19:31:20 +1300

    feat: add reducer, add test

---
+++ src/state/actions.ts
@@ -0,0 +1,41 @@
+import { ParchmentSectionKey, ParchmentSectionRef } from '../types.ts';
+
+export const PARCHMENT_ACTION = {
+    SET_CURRENT_PARCHMENT_SECTION: 'SET_CURRENT_PARCHMENT_SECTION',
+    ADD_PARCHMENT_SECTION: 'ADD_PARCHMENT_SECTION',
+    REMOVE_PARCHMENT_SECTION: 'REMOVE_PARCHMENT_SECTION',
+    SET_PARCHMENT_SECTION_IS_IN_VIEWPORT: 'SET_PARCHMENT_SECTION_IS_IN_VIEWPORT',
+};
+export type ParchmentAction = typeof PARCHMENT_ACTION[keyof typeof PARCHMENT_ACTION];
+
+interface SetCurrentSectionAction {
+    type: ParchmentAction;
+    payload: ParchmentSectionKey | null;
+}
+
+interface AddSectionAction {
+    type: ParchmentAction;
+    payload: {
+        parchmentSectionKey: ParchmentSectionKey;
+        parchmentSection: ParchmentSectionRef;
+    };
+}
+
+interface RemoveSectionAction {
+    type: ParchmentAction;
+    payload: ParchmentSectionKey;
+}
+
+interface SetSectionIsInViewportAction {
+    type: ParchmentAction;
+    payload: {
+        parchmentSectionKey: ParchmentSectionKey;
+        isInViewport: boolean;
+    };
+}
+
+export type ParchmentReducerAction =
+    SetCurrentSectionAction
+    | AddSectionAction
+    | RemoveSectionAction
+    | SetSectionIsInViewportAction;
--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -1,54 +1,37 @@
-import { ParchmentSectionKey, ParchmentSectionRef } from '../types.ts';
 import isString from '../utils/isString.ts';
+import {
+    PARCHMENT_ACTION,
+    ParchmentReducerAction,
+} from './actions.ts';
+import {
+    ParchmentSectionKey,
+    ParchmentSectionRef,
+} from '../types.ts';
 
-export const PARCHMENT_ACTION = {
-    SET_CURRENT_PARCHMENT_SECTION: 'SET_CURRENT_PARCHMENT_SECTION',
-    ADD_PARCHMENT_SECTION: 'ADD_PARCHMENT_SECTION',
-    REMOVE_PARCHMENT_SECTION: 'REMOVE_PARCHMENT_SECTION',
-    SET_PARCHMENT_SECTION_IS_IN_VIEWPORT: 'SET_PARCHMENT_SECTION_IS_IN_VIEWPORT',
-};
+const {
+    SET_CURRENT_PARCHMENT_SECTION,
+    ADD_PARCHMENT_SECTION,
+    REMOVE_PARCHMENT_SECTION,
+    SET_PARCHMENT_SECTION_IS_IN_VIEWPORT,
+} = PARCHMENT_ACTION;
 
-export type ParchmentAction = typeof PARCHMENT_ACTION[keyof typeof PARCHMENT_ACTION];
-interface SetCurrentSectionAction {
-    type: ParchmentAction;
-    payload: ParchmentSectionKey | null;
-}
-interface AddSectionAction {
-    type: ParchmentAction;
-    payload: {
-        parchmentSectionKey: ParchmentSectionKey;
-        parchmentSection: ParchmentSectionRef;
-    };
-}
-interface RemoveSectionAction {
-    type: ParchmentAction;
-    payload: ParchmentSectionKey;
-}
-
-interface SetSectionIsInViewportAction {
-    type: ParchmentAction;
-    payload: {
-        parchmentSectionKey: ParchmentSectionKey;
-        isInViewport: boolean;
-    };
-}
-
-type ParchmentReducerAction = SetCurrentSectionAction | AddSectionAction | RemoveSectionAction | SetSectionIsInViewportAction;
-
-interface ParchmentState {
+export interface ParchmentState {
     currentParchmentSectionKey: ParchmentSectionKey | null;
-    parchmentSections: Record<ParchmentSectionKey, { ref: ParchmentSectionRef; isInViewport: boolean }>;
+    parchmentSections: Record<ParchmentSectionKey, {
+        ref: ParchmentSectionRef;
+        isInViewport: boolean;
+    }>;
 }
 
 export default function reducer(state: ParchmentState, { type, payload }: ParchmentReducerAction) {
     switch (type) {
-        case PARCHMENT_ACTION.SET_CURRENT_PARCHMENT_SECTION:
+        case SET_CURRENT_PARCHMENT_SECTION:
             return {
                 ...state,
                 currentParchmentSectionKey: payload,
             };
 
-        case PARCHMENT_ACTION.ADD_PARCHMENT_SECTION:
+        case ADD_PARCHMENT_SECTION:
             if (!payload || isString(payload) || !('parchmentSection' in payload)) {
                 return state;
             }
@@ -63,7 +46,7 @@ export default function reducer(state: ParchmentState, { type, payload }: Parchm
                 },
             };
 
-        case PARCHMENT_ACTION.REMOVE_PARCHMENT_SECTION:
+        case REMOVE_PARCHMENT_SECTION:
             if (!isString(payload)) {
                 return state;
             }
@@ -72,7 +55,7 @@ export default function reducer(state: ParchmentState, { type, payload }: Parchm
 
             return state;
 
-        case PARCHMENT_ACTION.SET_PARCHMENT_SECTION_IS_IN_VIEWPORT:
+        case SET_PARCHMENT_SECTION_IS_IN_VIEWPORT:
             if (!payload || isString(payload) || !('isInViewport' in payload)) {
                 return state;
             }
